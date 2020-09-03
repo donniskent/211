3f576506ab0c6c8d0f8bffd1b596f68619b274ad
donniskent <donniskent@users.noreply.github.com>
2020-09-03 14:06:59 -0400

    Bug fix

--- Lab1Project/src/TwoPlayerController.java
+++ Lab1Project/src/TwoPlayerController.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TwoPlayerController {
-
 	public static void main(String[] args) {
 		TwoPlayerController controller = new TwoPlayerController();
 		 controller.go();
