67d4b158cdf07f2f3461eb04f76196f4ee7a5889
donniskent <donniskent@users.noreply.github.com>
2020-09-21 10:28:15 -0400

    Added methods to unit test

--- Lab2Project/src/MazeTest.java
+++ Lab2Project/src/MazeTest.java
@@ -38,8 +38,70 @@ public class MazeTest {
 		}
 		
 	}
+	@Test 
+	public void solveTest() {
+		Maze maze = new Maze(5,6); 
+		for(int i = 0; i < 5; i++) {
+			maze.set('#', 0, i); 
+			maze.set('#', 4, i);
 	
+		}
+		maze.set('#', 1, 2);
+		maze.set('#', 1, 4);
+		maze.set('#', 2, 0);
+		maze.set('#', 2, 2);
+		maze.set('#', 2, 4);
+		maze.set('#', 3, 0);
+		maze.set('#', 3, 4);
+		maze.set('#', 4, 0);
+		maze.set('#', 4, 4);
+		for(int row = 0; row < maze.getHeight(); row++) {
+			for(int col = 0; col < maze.getWidth(); col++) {
+			if(maze.get(row, col) != '#') {
+				maze.set(' ', row, col);
+				
+			}
+			
+			}
+			
+		
+		}
+		String answer =	maze.solve(1, 0, 1, 3);
+		MazeTextView view = new MazeTextView(maze);
+		view.display();
+		if(!answer.equals("1,1,3,1,3,3,1,3")) {
+			System.out.println(answer);
+			fail("didnt work");
+		}
+	
+			String answer2 = maze.solve(0, 9, -3, 3); 
+			if (answer2 != null) {
+				fail("Start and end coordinates out of bounds. Expected null got " + answer2);
+			}
+	
+			Maze maze2 = new Maze(5,5);
+			for(int row2 = 0; row2 < maze.getHeight(); row2++) {
+				for(int col2 = 0; col2 < maze.getWidth(); col2++) {
+				
+					maze.set(' ', row2, col2);
+					
+				
+	}
 
 
 
 }
+			String answer3 = maze2.solve(1, 3, 0, 3);
+			MazeTextView view2 = new MazeTextView(maze2);
+			view2.display();
+			
+	
+	
+	}
+	
+
+
+
+
+}
+
