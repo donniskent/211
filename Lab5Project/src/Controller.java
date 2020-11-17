5cd31a034069c242975e75a3704d66b3ea5670cd
donniskent <donniskent@users.noreply.github.com>
2020-11-17 13:34:02 -0500

    Pre - unit test push

--- Lab5Project/src/Controller.java
+++ Lab5Project/src/Controller.java
@@ -6,162 +6,109 @@ import javax.swing.JFileChooser;
 
 public class Controller {
 
-	public static void main(String [] args) {
+	public static void main(String[] args) {
 		Controller controller = new Controller();
 		controller.go();
-		
-		
-		
+
 	}
 
 	public void go() {
-		// maybe put values into an array list, clear the array list after getting recursive function answer
-		//
 		
+
 		File directory = readDirectory();
-	// have to pull the first file in the directory and read in its values. 
-		File [] allFiles = directory.listFiles();
+		// have to pull the first file in the directory and read in its values.
+		File[] allFiles = directory.listFiles();
 		String[] strings;
 		int count = 0;
-		
-		
-		for(int k = 0; k < allFiles.length ; k++) {
-		try {	
-			Scanner scan = new Scanner(allFiles[k]);
-			int numNodes = scan.nextInt();
-			strings = new String[numNodes];  // gets how many nodes there are 
-			Graph graph = new Graph(numNodes);
-			scan.nextLine();
-			while(scan.hasNextLine()) {
-				strings[count] = scan.nextLine();
-			count++;
-			}
-			
-			
-			for(int n = 0; n < strings.length; n++) {
-				Scanner stringScan = new Scanner(strings[n]);
-				while (stringScan.hasNextInt()) {
-					graph.setEdgeValue(n, stringScan.nextInt(), 1);
-					}
-			
-			}
-			ArrayList<Integer> visitedNodes = new ArrayList<Integer>();
-			
-			
-			ArrayList<Integer> queue = new ArrayList<Integer>();
-			queue.add(0);
-			
-			
-				
-			
-			
-				boolean result = graph.solve(queue, visitedNodes);
-				System.out.print("File " + k + " result is ");
-			if(result == true) {
-				System.out.print("solvable");
-			}
-			else {
-				System.out.print("unsolvable");
-			}
-			
-			System.out.println("");
-			
-			
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		}
-		catch (Exception e ) {
-			System.out.println("Problem reading a file in the directory.");
-			
-		}
-		
-		
-		
-		
-		
-		
-		
-		
-		count = 0;
-		
-		
-		
-		}
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-	/*
-		Graph graph = new Graph(2);
-		graph.setEdgeValue(0, 1, 1);
+		Scanner stringScan;
+		Scanner scan;
+		for (int k = 0; k < allFiles.length; k++) {
+			try {
+				scan = new Scanner(allFiles[k]);
+				int numNodes = scan.nextInt();
+				strings = new String[numNodes]; // gets how many nodes there are
+				Graph graph = new Graph(numNodes);
+				scan.nextLine();
+				while (scan.hasNextLine()) {
+					strings[count] = scan.nextLine();
+					count++;
+				}
 
-		
-		
-		
-		int count = 0;
-		boolean result;
-		
-		
-		ArrayList<Integer> unvisitedNodes = new ArrayList<Integer>();
-		
-		
-		ArrayList<Integer> queue = new ArrayList<Integer>();
-		queue.add(0);
-		
-		
-		
-		result = graph.solve(queue, unvisitedNodes);
-		
-		
-		
-		System.out.println(result);
-		
-		
-		
-		
-		
-		*/
-	
-	
+				for (int n = 0; n < strings.length; n++) {
+					stringScan = new Scanner(strings[n]);
+					while (stringScan.hasNextInt()) {
+						graph.setEdgeValue(n, stringScan.nextInt(), 1);
+					}
+
+				}
+				ArrayList<Integer> visitedNodes = new ArrayList<Integer>();
+				for(int i = 0; i < graph.getVertices(); i ++) {
+				ArrayList<Integer> queue = new ArrayList<Integer>();
+				queue.add(i);
+
+				boolean result = graph.solve(queue, visitedNodes);
+				System.out.print("File " + allFiles[k].getName() + " starting node " + i + " result is ");
+				if (result == true) {
+					System.out.print("solvable");
+				} else {
+					System.out.print("unsolvable");
+				}
+
+				System.out.println("");
+				queue.clear();
+				visitedNodes.clear();
+				}
+			} catch (Exception e) {
+				System.out.println("Problem reading file " + allFiles[k] + " in the directory");
+
+			}
+
+			count = 0;
+
+		}
+
 	}
+
 	
-		// pull in directory 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	public File readDirectory() {
 		JFileChooser chooser = new JFileChooser();
 		chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-		
+
 		int returnVal = chooser.showOpenDialog(null);
-		
+
 		if (returnVal == JFileChooser.APPROVE_OPTION) {
 
 			File file = chooser.getSelectedFile();
 
 			return file;
-			
 
 		}
 
-		
 		return null;
-		
-		
-	}
-	
-	
-	
 
+	}
 
 }
--- Lab5Project/src/Graph.java
+++ Lab5Project/src/Graph.java
@@ -2,24 +2,23 @@ import java.util.ArrayList;
 
 public class Graph {
 	private int[][] graph;
-	private int vertices;  
-	public Graph (int vertices) {
+	private int vertices;
+
+	public Graph(int vertices) {
 		// constructor for graph should build the specific N x N matrix to be analyzed
 		this.vertices = vertices;
 		graph = new int[vertices][vertices];
 		setInitialGraph();
-		
-		
+
 	}
-	
+
 	public int getVertices() {
 		return vertices;
 	}
 
 	public void setEdgeValue(int row, int col, int value) {
 		graph[row][col] = value;
-		
-		
+
 	}
 
 	public int getEdgeValue(int row, int col) {
@@ -27,80 +26,60 @@ public class Graph {
 	}
 
 	public void setInitialGraph() {
-		for(int i = 0; i < vertices; i++) {
-			for(int j = 0; j < vertices; j++) {
-			
+		for (int i = 0; i < vertices; i++) {
+			for (int j = 0; j < vertices; j++) {
+
 				graph[i][j] = 0;
-			
-			
+
 			}
 		}
 	}
-	
-	
+
 	private ArrayList<Integer> nodeList(int current) {
 		ArrayList<Integer> queue = new ArrayList<Integer>();
-		for(int col = 0; col < vertices; col ++) {
-			
-			if(graph[current][col] == 1) {
+		for (int col = 0; col < vertices; col++) {
+
+			if (graph[current][col] == 1) {
 				queue.add(col);
 			}
 		}
 		return queue;
-	
-	
+
 	}
-	
-	
-	
-	
-	
-	// queue top = 0                    queue bottom = queue.size -1
+
+	// queue top = 0 queue bottom = queue.size -1
 	public boolean solve(ArrayList<Integer> queue, ArrayList<Integer> VisitedNodes) {
 		// breadth first, start by popping the queue, this will be currentNode
-		
+
 		// grab the front of the queue, this is currentNode
 		int currentNode = queue.get(0);
 		queue.remove(0);
-		
+
 		// add the adjacent vertices to the queue
-		for(int i = 0; i < nodeList(currentNode).size(); i++) {
-			queue.add(nodeList(currentNode).get(i));       // puts the adjacent nodes into the queue
+		for (int i = 0; i < nodeList(currentNode).size(); i++) {
+			queue.add(nodeList(currentNode).get(i)); // puts the adjacent nodes into the queue
 		}
-		if(!VisitedNodes.contains(currentNode)) {
-		VisitedNodes.add(currentNode);}
-		
-		
-		
-		// base case 1: visited nodes = vertices (have to make sure duplicates arent added to visited nodes
-	if(VisitedNodes.size() == vertices) {
-		return true;
-	}
-		
-		// base case 2: queue is empty but not all vertices have been reached 
-	if(queue.size() == 0 && (VisitedNodes.size() < vertices)) {
-		return false;
-	}	
-	
-	
-	
-	if(queue.size() == 1 && VisitedNodes.contains(queue.get(0)))
-	return false;
-		// general: 
-		
+		if (!VisitedNodes.contains(currentNode)) {
+			VisitedNodes.add(currentNode);
+		}
+
+		// base case 1: visited nodes = vertices (have to make sure duplicates arent
+		// added to visited nodes
+		if (VisitedNodes.size() == vertices) {
+			return true;
+		}
+
+		// base case 2: queue is empty but not all vertices have been reached
+		if (queue.size() == 0 && (VisitedNodes.size() < vertices)) {
+			return false;
+		}
+
+		if (queue.size() == 1 && VisitedNodes.contains(queue.get(0)))
+			return false;
+		// general:
+
 		return solve(queue, VisitedNodes);
-		
-		
-		
-		
-		
-		
-		
-}
-	
-	
 
-
-
+	}
 
 }
